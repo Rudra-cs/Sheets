a056e2dbc389359eff51e7571bbaa2e7e6e518ee
Rudra <rudra.behera50@gmail.com>
2024-01-04 00:34:58 +0530

    fix: fixed the grid in the excel

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let cols = 26;
 
 let addressColCont = document.querySelector(".address-col-cont");
 let addressRowCont = document.querySelector(".address-row-cont");
+let cellsCont = document.querySelector(".cells-cont");
+let addressBar = document.querySelector(".address-bar");
 
 for (let i = 0; i < rows; i++) {
   let addressCol = document.createElement("div");
@@ -12,8 +14,30 @@ for (let i = 0; i < rows; i++) {
 }
 
 for (let i = 0; i < cols; i++) {
-  let addressCol = document.createElement("div");
-  addressCol.setAttribute("class", "address-col");
-  addressCol.innerText = i + 1;
-  addressRowCont.appendChild(addressCol);
+  let addressRow = document.createElement("div");
+  addressRow.setAttribute("class", "address-row");
+  addressRow.innerText = String.fromCharCode(65 + i);
+  addressRowCont.appendChild(addressRow);
+}
+
+for (let i = 0; i < rows; i++) {
+  let rowCont = document.createElement("div");
+  rowCont.setAttribute("class", "row-cont");
+  for (let j = 0; j < cols; j++) {
+    let cell = document.createElement("div");
+    cell.setAttribute("class", "cell");
+    cell.setAttribute("contenteditable", "true");
+    rowCont.appendChild(cell);
+    addListenerForAddressBarDisplay(cell, i, j);
+  }
+  cellsCont.appendChild(rowCont);
+}
+
+function addListenerForAddressBarDisplay(cell, i, j) {
+  cell.addEventListener("click", (e) => {
+    let rowId = i + 1;
+    let colId = String.fromCharCode(65 + j);
+
+    addressBar.value = `${colId}${rowId}`;
+  });
 }
